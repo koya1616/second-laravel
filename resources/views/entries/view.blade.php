ebb7786f19c96014622a23129a1a295f1d749e55
koya1616 <87183507+koya1616@users.noreply.github.com>
2021-10-16 22:24:52 +0900

    first commit

---
+++ resources/views/entries/view.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', $entry->title . ' - Blog')
+
+@include('layouts.header')
+
+@section('content')
+    <h1>{{ $entry->title }}</h1>
+    <article>
+        {{ $entry->content }}
+    <article>
+@endsection        
